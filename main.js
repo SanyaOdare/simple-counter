8c85a601da2cba5b62b54f2b81cbd80610111d7e
Sanya Oluwadare <sanyaodare@gmail.com>
2021-08-14 22:52:38 +0100

    set counter colors

--- main.js
+++ main.js
@@ -7,6 +7,23 @@ const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function (btn) {
   btn.addEventListener('click', function(e) {
-    console.log(e.currentTarget.classList);
-  })
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = 'green';
+    }
+    if (count < 0) {
+      value.style.color = 'red';
+    }
+    if (count === 0) {
+      value.style.color = '#222';
+    }
+    value.textContent = count;
+  });
 });
